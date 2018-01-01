dffd44a6647f115b0162a9882b16428ba052684b
Jan Nowak <jannowak@example.com>
2018-01-01 01:24:43 +0100

    add show/edit actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Succes Updated"
+      redirect_to article_path(@article)
+
+    else
+render 'edit'
+    end
   end
 
   def show
@@ -11,7 +30,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article created"
-      redirect_to @article
+      redirect_to article_path(@article)
     else
       render 'new'
     end
