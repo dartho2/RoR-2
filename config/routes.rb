46cb6f57dd18f531bd4d1615aa8d543731d92ca4
Jan Nowak <jannowak@example.com>
2018-01-03 12:01:38 +0100

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
-  resources :users
+  post 'login', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  resources :users, except: [:new]
   root "articles#index"
 end
